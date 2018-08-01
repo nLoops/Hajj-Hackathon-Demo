cd6120970c394762384414d9697cb3e02f2ad595
Ahmed Ibrahim <as01220767994@gmail.com>
2018-08-01 23:35:59 +0300

    feat: add draw routes lines between two point on map

--- app/src/main/java/com/nloops/lossless/HajiActivity.java
+++ app/src/main/java/com/nloops/lossless/HajiActivity.java
@@ -11,21 +11,36 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+import com.directions.route.AbstractRouting;
 import com.directions.route.Route;
 import com.directions.route.RouteException;
+import com.directions.route.Routing;
 import com.directions.route.RoutingListener;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.LocationCallback;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nloops.lossless.utilis.Constants;
 import java.util.ArrayList;
+import java.util.List;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class HajiActivity extends FragmentActivity implements OnMapReadyCallback,
@@ -36,11 +51,31 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
   private static final String TAG = HajiActivity.class.getSimpleName();
   /*final int code to ensure to get the permission for this specific activity.*/
   private static final int PERMISSION_REQ_CODE = 101;
+  /*this colors array will draw the routes*/
+  private static final int[] COLORS = new int[]{R.color.colorPrimary, R.color.colorAccent,
+      R.color.primary_dark_material_light};
+  /*ref of GoogleMap object*/
   private GoogleMap mMap;
+  /*ref of GoogleApiClient*/
   private GoogleApiClient mGoogleClient;
+  /*ref of Updated Location*/
   private Location mLocation;
+  /*ref of GuideLocation*/
+  private LatLng mGuideLocation;
+  /*ref of GoogleMap Location Request*/
   private LocationRequest mLocationRequest;
+  /*this flag to set first run Location*/
   private boolean isFirstLaunchFlag;
+  /*ref of Geofire to update guide current location in the Firebase DB*/
+  private GeoFire mGeoFire;
+  /*When Haji click on Marker this location will hold the Coordinates of guide location*/
+  private LatLng mRouteLocation;
+  /*Global Marker ref*/
+  private Marker guideMarker;
+  /*this arrayList will draw the route between two points*/
+  private List<Polyline> polylines;
+  /*Test location for testing routes*/
+  private LatLng testLocation;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +83,8 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
     setContentView(R.layout.activity_haji);
     /*first we need to get user permissions to access his location*/
     getPermissions();
+    /*init Poly ArrayList*/
+    polylines = new ArrayList<>();
     /*Set flag true*/
     isFirstLaunchFlag = true;
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -89,9 +126,31 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
         != PackageManager.PERMISSION_GRANTED) {
       return;
     }
-    //setLocationsMarker();
+    testLocation = new LatLng(21.618585, 39.158023);
+    mMap.addMarker(new MarkerOptions().position(testLocation).title("Test Position"));
     buildGoogleApiClient();
     mMap.setMyLocationEnabled(true);
+
+    mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+      @Override
+      public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        mRouteLocation = marker.getPosition();
+        return true;
+      }
+    });
+  }
+
+  public void setLostRequest(View view) {
+    LatLng myLocation = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+    Routing routing = new Routing.Builder()
+        .travelMode(AbstractRouting.TravelMode.DRIVING)
+        .withListener(this)
+        .alternativeRoutes(true)
+        .waypoints(myLocation, testLocation)
+        .build();
+    Log.i(TAG, "setLostRequest: " + myLocation.toString() + " " + testLocation.toString());
+    routing.execute();
   }
 
   /**
@@ -108,6 +167,11 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
   }
 
 
+  /**
+   * When Connecting with {@link #mGoogleClient} this method will setup our LocationRequest.
+   *
+   * @param bundle {@link Bundle}
+   */
   @Override
   public void onConnected(@Nullable Bundle bundle) {
     mLocationRequest = new LocationRequest();
@@ -126,7 +190,11 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
         , HajiActivity.this);
   }
 
-
+  /**
+   * This method will called every time location updates.
+   *
+   * @param location {@link Location}
+   */
   @Override
   public void onLocationChanged(Location location) {
     /*Set location to current updated location*/
@@ -135,21 +203,41 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
       isFirstLaunchFlag = false;
       /*Convert user coordinates into LatLng object.*/
       LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-      mMap.addMarker(new MarkerOptions().position(userLatLng)
+      Marker userMarker = mMap.addMarker(new MarkerOptions().position(userLatLng)
           .title(getString(R.string.user_your_location)));
+      userMarker.showInfoWindow();
       mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
       mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
     }
+
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    mGeoFire = new GeoFire(mDatabase.getReference(Constants.GUIDE_GENERAL_REF));
+    mGeoFire.getLocation(Constants.GUIDE_GEO_FIRE_KEY, new LocationCallback() {
+      @Override
+      public void onLocationResult(String key, GeoLocation location) {
+        if (guideMarker != null) {
+          guideMarker.remove();
+        }
+        mGuideLocation = new LatLng(location.latitude, location.longitude);
+        guideMarker = mMap.addMarker(new MarkerOptions()
+            .position(mGuideLocation).title("Your Guide"));
+
+      }
+
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
   }
-
-  private void setLocationsMarker() {
-
-  }
-
 
   @Override
   public void onRoutingFailure(RouteException e) {
-
+    if (e != null) {
+      Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+    } else {
+      Toast.makeText(this, "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
+    }
   }
 
   @Override
@@ -158,8 +246,48 @@ public class HajiActivity extends FragmentActivity implements OnMapReadyCallback
   }
 
   @Override
-  public void onRoutingSuccess(ArrayList<Route> arrayList, int i) {
+  public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+    LatLng latLng = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+    CameraUpdate center = CameraUpdateFactory.newLatLng(latLng);
+    CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
 
+    mMap.moveCamera(center);
+
+    if (polylines.size() > 0) {
+      for (Polyline poly : polylines) {
+        poly.remove();
+      }
+    }
+
+    polylines = new ArrayList<>();
+    //add route(s) to the map.
+    for (int i = 0; i < route.size(); i++) {
+
+      //In case of more than 5 alternative routes
+      int colorIndex = i % COLORS.length;
+
+      PolylineOptions polyOptions = new PolylineOptions();
+      polyOptions.color(getResources().getColor(COLORS[colorIndex]));
+      polyOptions.width(10 + i * 3);
+      polyOptions.addAll(route.get(i).getPoints());
+      Polyline polyline = mMap.addPolyline(polyOptions);
+      polylines.add(polyline);
+
+      Toast.makeText(getApplicationContext(),
+          "Route " + (i + 1) + ": distance - " + route.get(i).getDistanceValue() + ": duration - "
+              + route.get(i).getDurationValue(), Toast.LENGTH_SHORT).show();
+    }
+
+    // Start marker
+    MarkerOptions options = new MarkerOptions();
+    options.position(latLng);
+    mMap.addMarker(options);
+
+    // End marker
+    options = new MarkerOptions();
+    options.position(mGuideLocation);
+    //options.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_map));
+    mMap.addMarker(options);
   }
 
   @Override
