76ac004c7dff9b8bdd41730809cc0378d10818ec
Ahmed Ibrahim <as01220767994@gmail.com>
2018-08-01 23:36:19 +0300

    feat: add helper classes.

---
+++ app/src/main/java/com/nloops/lossless/utilis/Constants.java
@@ -0,0 +1,12 @@
+package com.nloops.lossless.utilis;
+
+public class Constants {
+
+  public static final String GUIDE_GEO_FIRE_KEY = "guide_location";
+  public static final String GUIDE_GENERAL_REF = "Guide";
+
+  /*private constructor to prevent calling*/
+  private Constants() {
+  }
+
+}
---
+++ app/src/main/java/com/nloops/lossless/utilis/Utilis.java
@@ -0,0 +1,23 @@
+package com.nloops.lossless.utilis;
+
+import android.content.Context;
+import com.nloops.lossless.R;
+import com.nloops.lossless.cards.CardModel;
+import java.util.ArrayList;
+
+public class Utilis {
+
+  /*private constructor to prevent calling*/
+  private Utilis() {
+  }
+
+  public static ArrayList<CardModel> createListModels(Context context) {
+    ArrayList<CardModel> models = new ArrayList<>();
+    models.add(new CardModel(context.getString(R.string.str_mock_haji_name),
+        context.getString(R.string.str_moc_init_status), R.mipmap.ic_launcher));
+    models.add(new CardModel(context.getString(R.string.str_mock_guide_name),
+        context.getString(R.string.str_moc_init_status), R.mipmap.ic_launcher));
+    return models;
+  }
+
+}
