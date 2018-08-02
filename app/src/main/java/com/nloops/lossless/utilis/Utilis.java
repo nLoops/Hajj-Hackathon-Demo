fd8ee2de4c50f7e020a31440cc58a36c879c2b44
Ahmed Ibrahim <as01220767994@gmail.com>
2018-08-02 05:42:30 +0300

    feat: Edit Model Image Drawables.

--- app/src/main/java/com/nloops/lossless/utilis/Utilis.java
+++ app/src/main/java/com/nloops/lossless/utilis/Utilis.java
@@ -14,9 +14,9 @@ public class Utilis {
   public static ArrayList<CardModel> createListModels(Context context) {
     ArrayList<CardModel> models = new ArrayList<>();
     models.add(new CardModel(context.getString(R.string.str_mock_haji_name),
-        context.getString(R.string.str_moc_init_status), R.mipmap.ic_launcher));
+        context.getString(R.string.str_moc_init_status), R.drawable.ic_map));
     models.add(new CardModel(context.getString(R.string.str_mock_guide_name),
-        context.getString(R.string.str_moc_init_status), R.mipmap.ic_launcher));
+        context.getString(R.string.str_moc_init_status), R.mipmap.ic_fab_route));
     return models;
   }
 
